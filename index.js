739751535ec7f3da1bda9f3d2e0be08081fbbe95
Lalit Singh <132752354+ltlalit15@users.noreply.github.com>
2025-09-27 13:36:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(cors({
   origin: ["https://bizcapitalsa.com", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+
 }));
 
 // ✅ Handle preflight requests
